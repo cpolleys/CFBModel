5075a58f7c894ade1f50a06dd8d46017b189a8fa
cpolleys <christopher.polleys@gmail.com>
2019-08-14 00:11:29 -0400

    adding scoreboard html file and update data file

--- data.py
+++ data.py
@@ -1,12 +1,17 @@
 from bs4 import BeautifulSoup
 import numpy as np
 import requests
+from selenium import webdriver
 
-r = requests.get('https://www.espn.com/college-football/scoreboard')
-soup = BeautifulSoup(r.text,'lxml')
-divs = soup.find_all('div')
-for div in divs:
-    print(div.find_all('section'))
+#driver = webdriver.Chrome()
+#driver.get('https://www.espn.com/college-football/scoreboard')
+#html = driver.page_source
+#soup = BeautifulSoup(html)
+
+page = open('Webpages\scoreboard.html', encoding = 'utf8').read()
+soup = BeautifulSoup(page,'lxml')
+for tag in soup.find_all('a')
+print(sheesh)
 
 #print(gamecast)
 #games = soup.find_all('div',{'class':'game'})
